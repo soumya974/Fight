19795f46913cd1ce366913b8c8ae49cdcf6f551e
soumya974 <56791208+soumya974@users.noreply.github.com>
2020-08-14 19:17:55 +0530

    Add files via upload

---
+++ Enemie.js
@@ -0,0 +1,10 @@
+class Enemie  {
+    constructor(x,y,width,height){
+      super(x,y,width,height);
+      this.image = loadImage("images/fight.jpg");
+    }
+  
+    display() {
+      super.display();
+    }
+  }     
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Fight
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
